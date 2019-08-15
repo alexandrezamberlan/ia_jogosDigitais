034bd01ab27ec770d64c245cb93f275af24bf3e7
Laboratório 09 <usrlab09@lanfran.local>
2019-08-15 17:45:41 -0300

    código C# para gerar labirintos em matrizes e em forms equivalentes

---
+++ c#/Labirinto/Labirinto/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Labirinto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gerarLabirintoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel_matriz = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gerarLabirintoToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // gerarLabirintoToolStripMenuItem
+            // 
+            this.gerarLabirintoToolStripMenuItem.Name = "gerarLabirintoToolStripMenuItem";
+            this.gerarLabirintoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gerarLabirintoToolStripMenuItem.Text = "Gerar Labirinto";
+            this.gerarLabirintoToolStripMenuItem.Click += new System.EventHandler(this.gerarLabirintoToolStripMenuItem_Click);
+            // 
+            // panel_matriz
+            // 
+            this.panel_matriz.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_matriz.Location = new System.Drawing.Point(0, 24);
+            this.panel_matriz.Name = "panel_matriz";
+            this.panel_matriz.Size = new System.Drawing.Size(800, 426);
+            this.panel_matriz.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel_matriz);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Labirinto";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gerarLabirintoToolStripMenuItem;
+        private System.Windows.Forms.Panel panel_matriz;
+    }
+}
+
---
+++ c#/Labirinto/Labirinto/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labirinto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gerarLabirintoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            qtdLinhas = panel_matriz.Height;
+            qtdColunas = panel_matriz.Width;
+            matriz = new int[qtdLinhas, qtdColunas];
+
+            MessageBox.Show(qtdLinhas + "x" + qtdColunas);
+
+            //inicializar a matriz
+            for (int i = 0; i < qtdLinhas; i++)
+            {
+                for (int j = 0; j < qtdColunas; j++)
+                {
+                    matriz[i,j] = 0;
+                }
+            }
+            //MessageBox.Show("Dimensão matriz: " + matriz.Length);
+
+            //sortear 1s em 10% da matriz, representando paredes
+            int percentualLabirinto = 1;
+            Random gerador = new Random();
+            int linha;
+            int coluna;
+            for (int i = 0; i < 500; i++)
+            {
+                linha = gerador.Next(matriz.GetLength(0));
+                coluna = gerador.Next(matriz.GetLength(1));
+                matriz[linha, coluna] = 1;
+            }
+            //plotar ou desenhar o labirinto
+            //botões representarão as paredes.
+            List<Button> paredes = new List<Button>();
+
+            for (int i = 0; i < qtdLinhas; i++)
+            {
+                for (int j = 0; j < qtdColunas; j++)
+                {
+                    if (matriz[i,j] == 1)
+                    {
+                        paredes.Add(new Button());
+
+                        paredes.Last().SetBounds(j, i, 5, 5);
+                        paredes.Last().Text = j + "," + i;
+
+                        panel_matriz.Controls.Add(paredes.Last());
+                    }
+                }
+            }
+
+            button_jogador.SetBounds(0,0,15,15);
+            //button_jogador.SetBounds.BackColor = "red";
+
+            panel_matriz.Controls.Add(button_jogador);
+
+            
+        }
+
+        int qtdLinhas;
+        int qtdColunas;
+        int[,] matriz;
+        Button button_jogador = new Button();
+
+    }
+}
