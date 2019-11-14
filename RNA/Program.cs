36c70c3f6acdc3c646f4502e4e9f0e39cb3bb095
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2019-11-14 16:33:44 -0300

    construção inicial de um perceptron em c#

---
+++ RNA/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RNA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de uso de RNA Perceptron!");
+            Perceptron_And p = new Perceptron_And();
+            p.treinar();
+ 
+            Console.WriteLine("Para aprender o algoritmo treinou " + p.conta + " geracoes! \n ");
+ 
+            p.testar();
+        }
+    }
+}
