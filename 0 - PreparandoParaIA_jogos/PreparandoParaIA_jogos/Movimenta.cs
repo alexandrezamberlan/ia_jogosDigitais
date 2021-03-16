297d9808c62dc2ef7257c06e3c9667733c9f40c9
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2021-03-16 17:23:02 -0300

    revisão matrizes

---
+++ 0 - PreparandoParaIA_jogos/PreparandoParaIA_jogos/Movimenta.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace PreparandoParaIA_jogos
+{
+    public class Movimenta
+    {
+
+        public static void moverCima(int[,] matriz, int dimensao)
+        {
+            int linhaZero = 0;
+            int colunaZero = 0;
+
+            for (int i = 0; i < dimensao; i++)
+            {
+                for (int j = 0; j < dimensao; j++)
+                {
+                    if (matriz[i,j] == 0)
+                    {
+                        linhaZero = i;
+                        colunaZero = j;
+                    }
+                }
+            }
+            Console.WriteLine("Posição do zero: " + linhaZero + "," + colunaZero);
+
+            if (linhaZero > 0)
+            {
+                matriz[linhaZero, colunaZero] = matriz[linhaZero-1, colunaZero];
+                matriz[linhaZero-1, colunaZero] = 0;
+
+                linhaZero--;
+            }
+        }
+
+        public static void moverBaixo(int[,] matriz, int dimensao)
+        {
+            int linhaZero = 0;
+            int colunaZero = 0;
+
+            for (int i = 0; i < dimensao; i++)
+            {
+                for (int j = 0; j < dimensao; j++)
+                {
+                    if (matriz[i,j] == 0)
+                    {
+                        linhaZero = i;
+                        colunaZero = j;
+                    }
+                }
+            }
+            Console.WriteLine("Posição do zero: " + linhaZero + "," + colunaZero);
+
+            if (linhaZero < dimensao - 1)
+            {
+                matriz[linhaZero, colunaZero] = matriz[linhaZero+1, colunaZero];
+                matriz[linhaZero+1, colunaZero] = 0;
+
+                linhaZero++;
+            }
+        }
+    }
+}
---
+++ 0 - PreparandoParaIA_jogos/PreparandoParaIA_jogos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PreparandoParaIA_jogos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int TAM = 5;
+            int[,] matriz = new int[TAM, TAM];
+
+            Util.sortearNumerosMatriz(matriz, TAM);
+
+            Util.mostrarMatriz(matriz, TAM);
+
+            char letra;
+            int x;
+            
+            do {
+                x = Console.Read();
+                letra = Convert.ToChar(x);
+                switch (letra)
+                {   
+                    case 'w' :
+                        Console.WriteLine("Indo para cima");
+                        Movimenta.moverCima(matriz, TAM);
+                        break;
+                    case 's' :
+                        Console.WriteLine("Indo para cima");
+                        Movimenta.moverBaixo(matriz, TAM);
+                        break;
+                    
+                }
+                Util.mostrarMatriz(matriz, TAM);
+            } while (letra != '1');
+        }
+    }
+}
