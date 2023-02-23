27dd7fab5f08d2a93899eb6f0edd5c6cd17751f1
Alexandre de O. Zamberlan <zamberlam@gmail.com>
2023-02-22 21:57:02 -0300

    Add files via upload

---
+++ 1 - MetodosBusca/ConsoleApp_Busca/Grafo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Busca
+{
+    internal class Grafo
+    {
+        public int QuantidadeVertices { get; set; }
+        public int QuantidadeArestas { get; set; }
+        public int[,] MatrizAdjacencia { get; set; }
+
+        public Grafo(int quantidadeVertices)
+        {
+            QuantidadeArestas = 0;
+            QuantidadeVertices = quantidadeVertices;
+
+            MatrizAdjacencia = new int[QuantidadeVertices, QuantidadeVertices];
+
+            for (int i = 0; i < QuantidadeVertices; i++)
+            {
+                for (int j = 0; j < QuantidadeVertices; j++)
+                {
+                    MatrizAdjacencia[i, j]  = 0;
+                }
+            }
+        }
+
+        public void exibirGrafo(List<string> lista)
+        {
+            
+            for (int i = 0; i < QuantidadeVertices; i++)
+            {
+                Console.Write(lista[i] + ": ");
+                for (int j = 0; j < QuantidadeVertices; j++)
+                {
+                    if (MatrizAdjacencia[i, j] != 0)
+                    {
+                        Console.Write(lista[j] +"(" + MatrizAdjacencia[i, j] + ") ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void inserirAssimetrico(int origem, int destino, int custo)
+        {
+            if (MatrizAdjacencia[origem, destino] == 0)
+            {
+                MatrizAdjacencia[origem, destino] = custo;
+                QuantidadeArestas++;
+            }
+        }
+    }
+}
---
+++ 1 - MetodosBusca/ConsoleApp_Busca/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp_Busca;
+
+Grafo exemplo1 = new Grafo(5);
+List<string> cidades = new List<string>();
+cidades.Add("a");
+cidades.Add("b");
+cidades.Add("c");
+cidades.Add("d");
+cidades.Add("e");
+
+//a = 0; b = 1; c = 2, d = 3, e = 4
+exemplo1.inserirAssimetrico(cidades.IndexOf("a"), cidades.IndexOf("b"), 10);
+exemplo1.inserirAssimetrico(cidades.IndexOf("b"), cidades.IndexOf("a"), 10);
+exemplo1.inserirAssimetrico(cidades.IndexOf("b"), cidades.IndexOf("d"), 20);
+exemplo1.inserirAssimetrico(cidades.IndexOf("c"), cidades.IndexOf("e"), 10);
+exemplo1.inserirAssimetrico(cidades.IndexOf("d"), cidades.IndexOf("c"), 40);
+exemplo1.inserirAssimetrico(cidades.IndexOf("d"), cidades.IndexOf("e"), 20);
+
+exemplo1.exibirGrafo(cidades);
