427b8ec8317858228213a8e51274d7a73c2bd7ab
Alexandre de O. Zamberlan <zamberlam@gmail.com>
2023-04-20 19:59:24 -0300

    Add files via upload

---
+++ 3 - AlgoritmosGeneticos/Rainhas_Game/Rainhas_Game/Program.cs
@@ -0,0 +1,21 @@
+﻿using Rainhas_Game;
+
+
+int tamanhoPopulacao = 100000;
+List<Matriz> listaTabuleiros = new List<Matriz>();  
+for (int i = 0; i < tamanhoPopulacao; i++)
+{
+    listaTabuleiros.Add( new Matriz(5) );
+    listaTabuleiros[i].preencherMatriz();
+    listaTabuleiros[i].verificarRestricoes();
+    listaTabuleiros[i].exibirMatriz("Tabuleiro...." + i);
+    if (listaTabuleiros[i].ehMeta())
+    {
+        Console.WriteLine("Achou!!!!!");
+        break;
+    }
+
+
+}
+
+
